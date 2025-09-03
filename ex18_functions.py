b014aa55ebe2731877dcc541c688062f2c655c47
Mudyy <43378293+Mudyy@users.noreply.github.com>
2025-09-03 15:16:10 +0500

    Create ex18_functions.py

---
+++ ex18_functions.py
@@ -0,0 +1,18 @@
+def do_nothing():
+    pass
+#this code will do nothing
+
+def do_something():
+    print("I did something")
+
+def do_more_things(a,b):
+    print("A IS",a,"B IS",b)
+
+
+"""def do_more_things(a,b):
+    a = "hello"
+    b = 1
+    print("A IS", a, "B IS", b)
+"""
+do_something()
+do_more_things("hello",1)
